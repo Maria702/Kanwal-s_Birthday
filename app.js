6d6da20adde2b6ebf18cc88d1cccc5e5b9cfe959
Maria Shaikh <mariamryw702@gmail.com>
2020-07-28 21:26:55 +0500

    birthday

---
+++ app.js
@@ -0,0 +1,12 @@
+var p = document.getElementById("para")
+var para = prompt("Enter your name");
+if (para === 'kanwal'){
+    alert('welcome')
+
+}
+
+else{
+    alert('Enter correct name');
+    
+   
+}
---
+++ app1.js
@@ -0,0 +1,10 @@
+
+function changeImg(){
+    var img = document.getElementById('img');
+    img.src = 'img/knwl4.jpg'
+}
+ function changeBef(){
+   var img = document.getElementById('img');
+   img.src = 'img/knwl3.jpg'
+}
+
